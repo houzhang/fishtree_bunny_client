f842cdd61ceafe0b40f76704641039510de43711
Houzhang Liang <houzhang@fishtree.com>
2015-03-06 16:37:10 +0000

    added missing require

--- lib/fishtree_bunny_client.rb
+++ lib/fishtree_bunny_client.rb
@@ -1,6 +1,7 @@
 require "bunny"
 require "yaml"
 require "fishtree_bunny_client/version"
+require "listener/queue_listener"
 
 Rabbit = YAML.load(File.read(File.expand_path('config/rabbitmq.yml')))
 Rabbit.merge! Rabbit.fetch(ENV['RACK_ENV'], {}).each_with_object({}){|(k,v), h| h[k.to_sym] = v}
